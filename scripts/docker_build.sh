16d49b69f793687e586d3749a2fb63ef7a2dd0ab
Elwardi <elwardifadeli@gmail.com>
2020-01-28 19:04:53 +0100

    Fix travis build: file permissions on git repo

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # Build and Test the toolkit in a container based off of foamscience/openrsr  docker image
-set -ev
 
 # Source FE4
 source /opt/foam/foam-extend-4.0/etc/bashrc
+set -ev
 
 # Get to the directory, compile libraries (Opt mode)
 cd /home/foam/OpenRSR; ./Allwmake
 
 # A full test of all library classes
-tests=`find . -iname "test" -type d`
+tests=`find /home/foam/OpenRSR -iname "test" -type d`
 for t in $tests; do
-    echo "Testing $t:\n"
+    echo "Testing $t:"
     pushd . > /dev/null
     cd $t
     wmake && ./*Test
