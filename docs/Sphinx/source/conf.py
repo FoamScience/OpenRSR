4fcd56e415e640aa56bb68780a80289423db0165
Elwardi <elwardifadeli@gmail.com>
2020-02-24 16:10:09 +0100

    Hook into builder init process

--- docs/Sphinx/source/conf.py
+++ docs/Sphinx/source/conf.py
@@ -105,7 +105,7 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
 
 import subprocess
-def builder_inited_handler(app):
+def config_inited_handler(config):
     cmd=[repo_root+'/docs/Sphinx/makeDoxygen']
     out = subprocess.Popen(cmd,
             stdout=subprocess.PIPE,
@@ -121,4 +121,4 @@ def builder_inited_handler(app):
     #    raise e
 
 def setup(app):
-    app.connect('builder-inited', builder_inited_handler)
+    app.connect('config-inited', config_inited_handler)
